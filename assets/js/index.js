69a80a86d2f6ad12a8e27d6f1f62a0c55e7782d9
tonyanguyen <tonyanguyen@berkeley.edu>
2017-11-26 23:22:50 -0800

    com4

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,13 @@
-$(document).ready(function() {
-	
-	$("#description").fadeIn(200);
+$(document).ready(function () {
+  $("#intro").click( function (){
+    if($(this).hasClass("helperClass")){
+     $(this).find("p").text("You found me!");
 
-	$(".menu-item").hover();
-	
-}
+    }else{
+      $(this).find("p").text("Hi, I'm Tonya");
+    }
+    $(this).toggleClass("helperClass");
+  });
+});                  
+
 
