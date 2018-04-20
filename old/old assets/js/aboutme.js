5686a76acf94ae1689f47647d50e469372460902
tonyanguyen <tonyanguyen@berkeley.edu>
2018-04-20 00:36:53 -0700

    child-parent was a little messed up, fixed

---
+++ old/old assets/js/aboutme.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('.menu-item').mouseenter(function () {
+        $(this).css({
+            outline: "0px solid transparent"
+        }).animate({
+            outlineWidth: '4px',
+            outlineColor: '#f37736'
+        }, 550);
+    }).mouseleave(function () {
+        $(this).animate({
+            outlineWidth: '0px',
+            outlineColor: '#037736'
+        }, 560);
+    });    
+})
